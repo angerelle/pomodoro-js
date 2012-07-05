230db97be740be37dda179305be2cedf67134474
Angela Relle <angela.relle@googlemail.com>
2012-07-05 23:28:23 +0100

    Working version of added fixture - when the start button is pressed, pomodoro should be started

---
+++ test/specs/PomodoroDomSpec.js
@@ -0,0 +1,40 @@
+/**
+ * PomodoroDomSpec.js
+ * @author Angela Relle
+ */
+
+describe("When the start button is pressed", function () {
+
+    var pomodoro;
+
+    beforeEach(function () {
+        pomodoro = new Pomodoro();
+        spyOn(pomodoro, 'start');
+
+        $('body').append('<div id="fixture"><button id="startButton" type="button">Start</button></div>') ;
+
+
+        var startButton = $('#startButton')[0];
+        startButton.onclick = function () {
+            startPomodoro();
+        };
+
+        startButton.click();
+
+
+    });
+
+    afterEach(function () {
+        $('#fixture').remove();
+    });
+
+
+
+    it("then the pomodoro should be started", function () {
+        expect(pomodoro.start).toHaveBeenCalled();
+    });
+
+    function startPomodoro() {
+        pomodoro.start();
+    }
+});
